40fc060ed65d1015605e252556b9dd1d79245787
arjun <arjun.mammen@cts.co>
2024-06-24 16:46:55 +0100

    pub/sub

--- data.tf
+++ data.tf
@@ -11,3 +11,8 @@ data "google_secret_manager_secret_version_access" "github-token-secret-version"
   project = var.project
   secret  = data.google_secret_manager_secret.github-token-secret.id
 }
+
+data "google_pubsub_topic" "data-metrics" {
+  name = "data-metrics"
+
+}
