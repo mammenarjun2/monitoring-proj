93107dc462d56902169417ad4bfae30dbb3817ad
arjun <arjun.mammen@cts.co>
2024-06-24 16:44:18 +0100

    pub/sub

--- logsink.tf
+++ logsink.tf
@@ -2,7 +2,7 @@ resource "google_logging_project_sink" "my-sink" {
   name = "metric-sink"
 
   # Can export to pubsub, cloud storage, bigquery, log bucket, or another project
-  destination = data.google_pubsuv_topic.data-metrics
+  destination = data.google_pubsub_topic.data-metrics
 
   # Log all WARN or higher severity messages relating to instances
   filter = "resource.type = gce_instance AND severity >= WARNING"
