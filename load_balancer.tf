0b743fd99ff6b79160efca9ed34df36a4c3f818a
arjun <mammenarjun2@gmail.com>
2024-07-01 11:00:19 +0000

    adding ldb/vm

--- load_balancer.tf
+++ load_balancer.tf
@@ -2,7 +2,7 @@ resource "google_compute_backened_service" {
   name = "ldb-4"
   protocol = "HTTP"
   load_balancing_scheme = "EXTERNAL"
-  health check = [google_compute_http_health_check.default.id]
+  health_check = [google_compute_http_health_check.default.id]
 }
 resource "google_compute_http_health_check" "ldb_check" {
   name               = "vm_liveness_check"
