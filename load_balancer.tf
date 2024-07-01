b198e350a4efed9b6388a75d00aaf84da18e5445
arjun <mammenarjun2@gmail.com>
2024-07-01 13:23:45 +0000

    zone update

--- load_balancer.tf
+++ load_balancer.tf
@@ -10,3 +10,24 @@ resource "google_compute_http_health_check" "ldb_check" {
   check_interval_sec = 1
   timeout_sec        = 1
 }
+
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend_service.self_link
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name    = "http-proxy"
+  url_map = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "http_forwarding_rule" {
+  name        = "http-forwarding-rule"
+  target      = google_compute_target_http_proxy.http_proxy.self_link
+  port_range  = "80"
+  ip_protocol = "TCP"
+}
+
+resource "google_compute_address" "global_address" {
+  name = "global-address"
+}
