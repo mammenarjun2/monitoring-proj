cc81515d37fadff2bdbc2d48921317dd7b2b0e73
arjun <mammenarjun2@gmail.com>
2024-07-02 09:48:13 +0000

    ldb/vm

--- load_balancer.tf
+++ load_balancer.tf
@@ -15,14 +15,14 @@ resource "google_compute_backend_service" "backend_service" {
   health_checks = [google_compute_http_health_check.ldb_check.id]
   
    backend {
-    group = data.google_compute_instance_group.instance_group_manager.id,
+    group = data.google_compute_instance_group.instance_group_manager.id
     balancing_mode = "UTILIZATION"
     capacity_scaler = 0.5
     max_utilization = 0.5
   },
 
    backend {
-    group = data.google_compute_instance_group.instance_group_manager2.id,
+    group = data.google_compute_instance_group.instance_group_manager2.id
     balancing_mode = "UTILIZATION"
     capacity_scaler = 0.5
     max_utilization = 0.5
