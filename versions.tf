cb91db07d92b3f8bf33388f9c08e2dec4afae466
arjun <mammenarjun2@gmail.com>
2024-07-03 09:03:15 +0000

    version

--- versions.tf
+++ versions.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.3"
 
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+  }
+
   backend "gcs" {}
 
 
